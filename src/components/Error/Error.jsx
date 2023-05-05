ea60b30f8536f9793bbac66173542fc8a32c119a
MD-Mojammel-Haque-Shakil <mojammelhaque8967@gmail.com>
2023-05-05 22:27:43 +0600

    some code modify

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './Error.css'
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 
 const Error = () => {
     return (
-       <>
-       <Header></Header>
+
         <div className='error'>
            <img className='errorImg' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk74G6dqh9xBo1D77JfeN-UlzUUR9V9SgQkQ&usqp=CAU" alt="" />
         </div>
-        <Footer></Footer>
-       </>
+
     );
 };
 
